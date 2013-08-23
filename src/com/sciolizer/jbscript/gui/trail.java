d26f331ec4303b223b88146d9ceadd8e7e88a15e
Joshua Ball <JoshBBall@gmail.com>
2013-08-22 22:00:47 -0700

    Separate ActionListeners.

--- src/com/sciolizer/jbscript/gui/trail.java
+++ src/com/sciolizer/jbscript/gui/trail.java
@@ -8,6 +8,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
@@ -18,15 +19,10 @@ import java.util.GregorianCalendar;
  * Time: 8:59 AM
  * To change this template use File | Settings | File Templates.
  */
-public class trail extends JFrame implements ActionListener {
+public class trail extends JFrame {
 
         MenuBar mbar;
         Menu file,edit,format,font,font1,font2;
-        MenuItem item1,item2,item3,item4;
-        MenuItem item5,item6,item7,item8,item9,item10;
-        MenuItem fname1,fname2,fname3,fname4;
-        MenuItem fstyle1,fstyle2,fstyle3,fstyle4;
-        MenuItem fsize1,fsize2,fsize3,fsize4;
 
         JPanel mainpanel;
         TextArea text;
@@ -75,67 +71,197 @@ public class trail extends JFrame implements ActionListener {
             font1=new Menu("Font Style");
             font2=new Menu("Size");
 
-            file.add(item1=new MenuItem("New..."));
-            file.add(item2=new MenuItem("Open"));
-            file.add(item3=new MenuItem("Save As..."));
-            file.add(item4=new MenuItem("Exit"));
+            addMenuChild(file, "New...", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    dispose();
+                    trail note1 = new trail("Untitled-Notepad");
+                    note1.setSize(500, 500);
+                    note1.setVisible(true);
+                }
+            });
+            addMenuChild(file, "Open", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    str4 = " ";
+                    FileDialog dialog = new FileDialog(trail.this, "Open");
+                    dialog.setVisible(true);
+
+                    str1 = dialog.getDirectory();
+                    str2 = dialog.getFile();
+                    str3 = str1 + str2;
+                    File f = new File(str3);
+                    try {
+                        FileInputStream fobj = new FileInputStream(f);
+                        len = (int) f.length();
+                        for (int j = 0; j < len; j++) {
+                            char str5 = (char) fobj.read();
+                            str4 = str4 + str5;
+
+                        }
+                    } catch (IOException e1) {
+                        e1.printStackTrace();
+                    }
+                    text.setText(str4);
+                }
+            });
+            addMenuChild(file, "Save As...", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {
+
+                        if (command.equals("Save As...")) {
+                            FileDialog dialog1 = new FileDialog(trail.this, "Save As", FileDialog.SAVE);
+                            dialog1.setVisible(true);
+
+                            str7 = dialog1.getDirectory();
+                            str8 = dialog1.getFile();
+                            str9 = str7 + str8;
+
+
+                            str6 = text.getText();
+                            len1 = str6.length();
+                            byte buf[] = str6.getBytes();
+
+                            File f1 = new File(str9);
+                            FileOutputStream fobj1 = new FileOutputStream(f1);
+                            for (int k = 0; k < len1; k++) {
+                                fobj1.write(buf[k]);
+                            }
+                            fobj1.close();
+                        }
+
+                        trail.this.setTitle(str8);
+
+                    } catch (IOException ee) {
+                        ee.printStackTrace();
+                    }
+
+                }
+            });
+            addMenuChild(file, "Exit", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+                }
+            });
+
             mbar.add(file);
 
 
-            edit.add(item5=new MenuItem("Cut (Ctrl+X)"));
-            edit.add(item6=new MenuItem("Copy (Ctrl+C)"));
-            edit.add(item7=new MenuItem("Paste (Ctrl+V)"));
-            edit.add(item8=new MenuItem("Delete"));
-            edit.add(item10=new MenuItem("Select All (Ctrl+A)"));
-            edit.add(item9=new MenuItem("Time/Date"));
+            addMenuChild(edit, "Cut (Ctrl+X)", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    trail.this.str = text.getSelectedText();
+                    i = text.getText().indexOf(trail.this.str);
+                    text.replaceRange(" ", i, i + trail.this.str.length());
+                }
+            });
+            addMenuChild(edit, "Copy (Ctrl+C)", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    trail.this.str = text.getSelectedText();
+                }
+            });
+            addMenuChild(edit, "Paste (Ctrl+V)", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    pos1=text.getCaretPosition();
+                    text.insert(trail.this.str,pos1);
+                }
+            });
+
+            addMenuChild(edit, "Delete", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String msg = text.getSelectedText();
+                    i = text.getText().indexOf(msg);
+                    text.replaceRange(" ", i, i + msg.length());
+                }
+            });
+            addMenuChild(edit, "Select All (Ctrl+A)", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String strText = text.getText();
+                    int strLen = strText.length();
+                    text.select(0, strLen);
+                }
+            });
+            addMenuChild(edit, "Time/Date", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    gcalendar=new GregorianCalendar();
+                    String h=String.valueOf(gcalendar.get(Calendar.HOUR));
+                    String m=String.valueOf(gcalendar.get(Calendar.MINUTE));
+                    String s=String.valueOf(gcalendar.get(Calendar.SECOND));
+                    String date=String.valueOf(gcalendar.get(Calendar.DATE));
+                    String mon=months[gcalendar.get(Calendar.MONTH)];
+                    String year=String.valueOf(gcalendar.get(Calendar.YEAR));
+                    String hms="Time"+" - "+h+":"+m+":"+s+" Date"+" - "+date+" "+mon+" "+year;
+                    int loc=text.getCaretPosition();
+                    text.insert(hms,loc);
+                }
+            });
             mbar.add(edit);
 
             format.add(font);
             format.add(font1);
             format.add(font2);
 
-            font.add(fname1=new MenuItem("Courier"));
-            font.add(fname2=new MenuItem("Sans Serif"));
-            font.add(fname3=new MenuItem("Monospaced"));
-            font.add(fname4=new MenuItem("Symbol"));
+            addMenuChild(font, "Courier", new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String fontName = f.getName();
+                    String fontFamily = f.getFamily();
+                    int fontSize = f.getSize();
+                    int fontStyle = f.getStyle();
 
-            font1.add(fstyle1=new MenuItem("Regular"));
-            font1.add(fstyle2=new MenuItem("Bold"));
-            font1.add(fstyle3=new MenuItem("Italic"));
-            font1.add(fstyle4=new MenuItem("Bold Italic"));
+                    f = new Font("Courier", fontStyle, fontSize);
+                    text.setFont(f);
+                }
+            });
 
-            font2.add(fsize1=new MenuItem("12"));
-            font2.add(fsize2=new MenuItem("14"));
-            font2.add(fsize3=new MenuItem("18"));
-            font2.add(fsize4=new MenuItem("20"));
+            ActionListener fontFaceAction = new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String fontName=f.getName();
+                    String fontFamily=f.getFamily();
+                    int fontSize=f.getSize();
+                    int fontStyle=f.getStyle();
+
+                    f=new Font(e.getActionCommand(),fontStyle,fontSize);
+                    text.setFont(f);
+                }
+            };
+            for (String face : Arrays.asList("Sans Serif", "Monospaced", "Symbol")) {
+                addMenuChild(font, face, fontFaceAction);
+            }
+
+            font1.add(menuItemFontStyle("Regular", Font.PLAIN));
+            font1.add(menuItemFontStyle("Bold", Font.BOLD));
+            font1.add(menuItemFontStyle("Italic", Font.ITALIC));
+            font1.add(menuItemFontStyle("Bold Italic", Font.BOLD|Font.ITALIC));
+
+            for (int fontSize : new int[]{12, 14, 18, 20}) {
+                MenuItem fsize = new MenuItem(String.valueOf(fontSize));
+                final int size = fontSize;
+                fsize.addActionListener(new ActionListener() {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String fontName=f.getName();
+                        String fontFamily=f.getFamily();
+                        int fontSize=f.getSize();
+                        int fontStyle=f.getStyle();
+
+                        f=new Font(fontName,fontStyle,size);
+                        text.setFont(f);
+                    }
+                });
+                font2.add(fsize);
+            }
 
             mbar.add(format);
-
-
-
-            item1.addActionListener(this);
-            item2.addActionListener(this);
-            item3.addActionListener(this);
-            item4.addActionListener(this);
-            item5.addActionListener(this);
-            item6.addActionListener(this);
-            item7.addActionListener(this);
-            item8.addActionListener(this);
-            item9.addActionListener(this);
-            item10.addActionListener(this);
-            fname1.addActionListener(this);
-            fname2.addActionListener(this);
-            fname3.addActionListener(this);
-            fname4.addActionListener(this);
-            fstyle1.addActionListener(this);
-            fstyle2.addActionListener(this);
-            fstyle3.addActionListener(this);
-            fstyle4.addActionListener(this);
-            fsize1.addActionListener(this);
-            fsize2.addActionListener(this);
-            fsize3.addActionListener(this);
-            fsize4.addActionListener(this);
-
 
             text=new TextArea(26,60);
             mainpanel.add(text);
@@ -144,264 +270,30 @@ public class trail extends JFrame implements ActionListener {
             text.setFont(f);
         }
 
-
-
-
-    public void actionPerformed(ActionEvent ae)
-    {
-
-
-        command=(String)ae.getActionCommand();
-
-        System.out.println("command: " + command);
-
-        if(command.equals("New..."))
-        {
-            dispose();
-            trail note1 = new trail("Untitled-Notepad");
-            note1.setSize(500,500);
-            note1.setVisible(true);
-        }
-
-        try
-        {
-
-            if(command.equals("Open"))
-            {
-
-                str4=" ";
-                FileDialog dialog=new FileDialog(this,"Open");
-                dialog.setVisible(true);
-
-                str1=dialog.getDirectory();
-                str2=dialog.getFile();
-                str3=str1+str2;
-                File f=new File(str3);
-                FileInputStream fobj=new FileInputStream(f);
-                len=(int)f.length();
-                for(int j=0;j<len;j++)
-                {
-                    char str5=(char)fobj.read();
-                    str4=str4 + str5;
-
-                }
-
-                text.setText(str4);
-
-            }
-        }
-        catch(IOException e)
-        {}
-
-
-        try
-        {
-
-            if(command.equals("Save As..."))
-            {
-                FileDialog dialog1=new FileDialog(this,"Save As",FileDialog.SAVE);
-                dialog1.setVisible(true);
-
-                str7=dialog1.getDirectory();
-                str8=dialog1.getFile();
-                str9=str7+str8;
-
-
-                str6=text.getText();
-                len1=str6.length();
-                byte buf[]=str6.getBytes();
-
-                File f1=new File(str9);
-                FileOutputStream fobj1=new FileOutputStream(f1);
-                for(int k=0;k<len1;k++)
-                {
-                    fobj1.write(buf[k]);
-                }
-                fobj1.close();
-            }
-
-            this.setTitle(str8);
-
-        }
-        catch(IOException e){}
-
-
-
-        if(command.equals("Exit"))
-        {
-            System.exit(0);
-        }
-
-        if(command.equals("Cut"))
-        {
-            str=text.getSelectedText();
-            i=text.getText().indexOf(str);
-            text.replaceRange(" ",i,i+str.length());
-        }
-
-        if(command.equals("Copy"))
-        {
-            str=text.getSelectedText();
-        }
-
-        if(command.equals("Paste"))
-        {
-            pos1=text.getCaretPosition();
-            text.insert(str,pos1);
-        }
-        if(command.equals("Delete"))
-        {
-            String msg=text.getSelectedText();
-            i=text.getText().indexOf(msg);
-            text.replaceRange(" ",i,i+msg.length());
-        }
-        if(command.equals("Time/Date"))
-        {
-            gcalendar=new GregorianCalendar();
-            String h=String.valueOf(gcalendar.get(Calendar.HOUR));
-            String m=String.valueOf(gcalendar.get(Calendar.MINUTE));
-            String s=String.valueOf(gcalendar.get(Calendar.SECOND));
-            String date=String.valueOf(gcalendar.get(Calendar.DATE));
-            String mon=months[gcalendar.get(Calendar.MONTH)];
-            String year=String.valueOf(gcalendar.get(Calendar.YEAR));
-            String hms="Time"+" - "+h+":"+m+":"+s+" Date"+" - "+date+" "+mon+" "+year;
-            int loc=text.getCaretPosition();
-            text.insert(hms,loc);
-        }
-        if(command.equals("Courier"))
-        {
-
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font("Courier",fontStyle,fontSize);
-            text.setFont(f);
-        }
-        if(command.equals("Sans Serif"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font("Sans Serif",fontStyle,fontSize);
-            text.setFont(f);
-        }
-        if(command.equals("Monospaced"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font("Monospaced",fontStyle,fontSize);
-            text.setFont(f);
-        }
-
-        if(command.equals("Symbol"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font("Symbol",fontStyle,fontSize);
-            text.setFont(f);
-            System.out.println(f.getFamily());
-        }
-        if(command.equals("Regular"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,Font.PLAIN,fontSize);
-            text.setFont(f);
-        }
-        if(command.equals("Bold"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,Font.BOLD,fontSize);
-            text.setFont(f);
-        }
-        if(command.equals("Italic"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,Font.ITALIC,fontSize);
-            text.setFont(f);
-        }
-        if(command.equals("Bold Italic"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,Font.BOLD|Font.ITALIC,fontSize);
-            text.setFont(f);
-        }
-
-        if(command.equals("12"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,fontStyle,12);
-            text.setFont(f);
-        }
-
-        if(command.equals("14"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,fontStyle,14);
-            text.setFont(f);
-        }
-        if(command.equals("18"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,fontStyle,18);
-            text.setFont(f);
-        }
-        if(command.equals("20"))
-        {
-            String fontName=f.getName();
-            String fontFamily=f.getFamily();
-            int fontSize=f.getSize();
-            int fontStyle=f.getStyle();
-
-            f=new Font(fontName,fontStyle,20);
-            text.setFont(f);
-        }
-        if(command.equals("Select All"))
-        {
-            String strText=text.getText();
-            int strLen=strText.length();
-            text.select(0,strLen);
-        }
-
-
+    private void addMenuChild(Menu parent, String caption, ActionListener action) {
+        MenuItem menuItem = new MenuItem(caption);
+        parent.add(menuItem);
+        menuItem.addActionListener(action);
     }
+
+    private MenuItem menuItemFontStyle(String caption, final int fontStyle) {
+        MenuItem ret = new MenuItem(caption);
+        ret.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String fontName=f.getName();
+                String fontFamily=f.getFamily();
+                int fontSize=f.getSize();
+//                int fontStyle=f.getStyle();
+
+                f=new Font(fontName,fontStyle,fontSize);
+                text.setFont(f);
+            }
+        });
+        return ret;
+    }
+
 
     public static void main(String args[])
     {
